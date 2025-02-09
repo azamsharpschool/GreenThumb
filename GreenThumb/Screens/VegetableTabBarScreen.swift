d907509ce9ea79919cb69b6bd75eddcb44726923
Mohammad Azam <azamsharp@gmail.com>
2025-02-09 14:08:35 -0600

    Adding VegetableTabBarScreen

---
+++ GreenThumb/Screens/VegetableTabBarScreen.swift
@@ -0,0 +1,41 @@
+//
+//  VegetableTabBarScreen.swift
+//  GreenThumb
+//
+//  Created by Mohammad Azam on 2/9/25.
+//
+
+import SwiftUI
+
+struct VegetableTabBarScreen: View {
+    var body: some View {
+        TabView {
+            
+            NavigationStack {
+                VegetableListScreen()
+            }.tabItem {
+                    Image(systemName: "leaf")
+                    Text("Vegetables")
+                }
+            
+            NavigationStack {
+                Text("MyGardenScreen")
+            }.tabItem {
+                    Image(systemName: "house")
+                    Text("My Garden")
+                }
+            
+            NavigationStack {
+                Text("Pests")
+            }.tabItem {
+                    Image(systemName: "ladybug")
+                    Text("Pests")
+                }
+            
+        }
+    }
+}
+
+#Preview {
+    VegetableTabBarScreen()
+}
